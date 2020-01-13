5366d13398576f344d5f66e8de3ed2736eb21422
Aaron <proaaron.714@gmail.com>
2020-01-13 15:50:52 -0500

    Update index

--- index.php
+++ index.php
@@ -47,21 +47,23 @@ an IMS Learning Tools Interoperability® (LTI®)
 </li>
 </ul>
 <?php } else { ?>
-<p>
-Hello and welcome to my site where you can work through modules and learn to write syntactically correct HTML5 and CSS3, and how to create interactive web experiences with JavaScript. Mastering this range of technologies will allow you to develop high quality web sites that, work seamlessly on mobile, tablet, and large screen browsers accessible.
+<p>Hello and welcome to my site where you can work through modules and learn to
+write syntactically correct HTML5 and CSS3, and how to create interactive web
+experiences with JavaScript. Mastering this range of technologies will allow you
+to develop high quality web sites that, work seamlessly on mobile, tablet,
+and large screen browsers accessible.</p>
 <br>
 You can use this web site many different ways:
 <ul>
-<li>
-You browse my videos and course materials under <a href="lessons">Lessons</a>. The materials
-I have developed for this class are all provided with a Creative Commons license so you can download or link to
-them to incorporate them into your own teaching if you like.</li>
-We take your privacy seriously on this site, you can review our
-<a href="privacy">Privacy Policy</a> for more details.
-</li>
+  <li>You can browse my videos and course materials under <a href="lessons">Lessons</a>.</li>
+  <li>The materials I have developed for this class are all provided with a
+  Creative Commons license so you can download or link to them to incorporate
+  them into your own teaching if you like.</li>
+  <li>We take your privacy seriously on this site, you can review our
+  <a href="privacy">Privacy Policy</a> for more details.</li>
 </ul>
 <?php } ?>
-This site uses <a href="http://www.tsugi.org" target="_blank">Tsugi</a>
+<p>This site uses <a href="http://www.tsugi.org" target="_blank">Tsugi</a>
 framework to embed a learning
 management system into this site and handle the autograders.
 If you are interested in collaborating
@@ -69,27 +71,16 @@ to build these kinds of sites for yourself, please see the
 <a href="http://www.tsugi.org" target="_blank">tsugi.org</a> website.
 </p>
 <h3>Textbook</h3>
-<p>
-I am using an open textbook
-<a href="https://learn.shayhowe.com/html-css/"
-target="_new">
-Learn to Code HTML & CSS
-</a>
-by Shay Howe.
-All of the electronic copies
+<p>I am using an open textbook<a href="https://learn.shayhowe.com/html-css/"
+target="_new">Learn to Code HTML & CSS</a> by Shay Howe. All of the electronic copies
 of the book are free. Print copies of the book are available on
 <a href="https://www.informit.com/store/learn-to-code-html-and-css-develop-and-style-websites-9780321940520?ranMID=24808"
-target="_blank">Pearson</a>.
-</p>
+target="_blank">Pearson</a>.</p>
 <h3>Copyright</h3>
-<p>
-All this material produced by Colleen Van Lent (including audio and video)
+<p>All this material produced by Colleen Van Lent (including audio and video)
 is Copyright Creative Commons Attribution 3.0
-unless otherwise indicated.
-</p>
-<p>
---  Colleen
-</p>
+unless otherwise indicated.</p>
+
 <!--
 <?php
 echo("IP Address: ".Net::getIP()."\n");
